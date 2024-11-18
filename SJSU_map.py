83def47574af641f3d91f3c799ab64abfba6f5a1
Nikita Khatwani <98651122+NikitaKhatwani@users.noreply.github.com>
2024-11-18 18:34:01 -0500

    Update SJSU_map.py

--- SJSU_map.py
+++ SJSU_map.py
@@ -749,7 +749,7 @@ def main():
             aggregate_monthly = pd.DataFrame()
 
             aggregate_monthly = final_df_CS[["CS Electricity Loads(kBtu)","CS Heating Loads(kBtu)","CS Cooling Loads(kBtu)","Simultaneuos Loads(H)","Simultaneuos Loads(C)"]]
-            # aggregate_monthly['timestamp'] = timestamp_TMY
+            aggregate_monthly['timestamp'] = timestamp_TMY
             # Assuming final_df_CS is your DataFrame and 'timestamp_TMY' is the timestamp column
             # aggregate_monthly['timestamp'] = pd.to_datetime(timestamp_TMY)  # Convert to datetime if not already
             # Set the timestamp as the index
